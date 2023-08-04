18376e05fb4f07bc7b05d29cb248d95903464896
Mitttttechka <mitttttechka@yandex.ru>
2023-08-04 14:51:38 +0200

    Initial bot (not working)

---
+++ keys.py
@@ -0,0 +1,2 @@
+token = '6447033075:AAH4YTMabrOEOjQgsSvlsHeV0kCTyLl73wQ'
+bot_name = 'learn_without_bs_bot'
--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+from typing import Final
+from telegram.ext import *
+import keys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print ('Starting up bot...')
 
+def start_command(update, context):
+    update.message.reply_text('Hello there, I\'m a bot')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def help_command(update, context):
+    update.message.reply_text('I\'ll help you')
 
+def custom_command(update, context):
+    update.message.reply_text('This is custom command')
 
-# Press the green button in the gutter to run the script.
+def handle_response(text: str) -> str:
+    if 'hello' in text:
+        return 'Hey there!'
+
+    if 'how are you' in text:
+        return 'I\'m good and you?'
+
+    return 'Idk'
+
+def handle_massage(update, context):
+    message_type = update.message.chat.type
+    text = str(update.message.text).lower()
+    response = ''
+
+    print(f'User ({update.message.chat.id}) says: "{text} in: {message_type}"')
+
+    if message_type == 'group':
+        if '@learn_without_bs_bot' in text:
+            new_text = text.replace('@learn_without_bs_bot', '').strip()
+            response = handle_response(new_text)
+        else:
+            response = handle_response(text)
+
+    update.message.reply_text(response)
+
+def error(update, context):
+    print(f'Update {update} caused error: {context.error}')
+
 if __name__ == '__main__':
-    print_hi('PyCharm!')
+    updater = Updater(keys.token, use_context = True)
+    dp = updater.dispatcher
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #commands
+    dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CommandHandler('help', start_command))
+    dp.add_handler(CommandHandler('custom', start_command))
+
+    #dp.add_handler(MessageHandler(Filters.text, handle_massage))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling(1.0)
+    updater.idle()
